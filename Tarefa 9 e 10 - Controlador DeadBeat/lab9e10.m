d91ac26a78f2d756eefc7f839c69c65ebc528aab
Hugo Nakamura <hugohiroyuki9@gmail.com>
2024-11-06 08:58:55 -0300

    Atualização tarefa 9 e 10

--- Tarefa 9 e 10 - Controlador DeadBeat/lab9e10.m
+++ Tarefa 9 e 10 - Controlador DeadBeat/lab9e10.m
@@ -5,10 +5,10 @@
 clc;
 close all;
 % VALORES PADRÕES.
-R = 1.25;
+R = 1;
 zeta = 1.011;
 wn = 1.045;
-T0 = 0.201;
+T0 = 0.377;
 
 d = -0.15; % disturbio
 td = 15; % tempo de inicio do disturbio
@@ -23,36 +23,36 @@ disp(H0GPnum)
 disp(H0GPden)
 
 % COEFICIENTES DA PLANTA COM ZERO HOLDER.
-b0 = H0GPnum(1);
-b1 = H0GPnum(2);
-b2 = H0GPnum(3);
+b0 = H0GPnum(1)
+b1 = H0GPnum(2)
+b2 = H0GPnum(3)
 
-a0 = H0GPden(1);
-a1 = H0GPden(2);
-a2 = H0GPden(3);
+a0 = H0GPden(1)
+a1 = H0GPden(2)
+a2 = H0GPden(3)
 
 % QUESTÃO 1:
 % Dead beat de ordem 2:
 
-q0 = 1/(b1+b2);
-q1 = a1*q0;
-q2 = a2*q0;
+q0 = 1/(b1+b2)
+q1 = a1*q0
+q2 = a2*q0
 
-p1 = b1*q0;
-p2 = b2*q0;
+p1 = b1*q0
+p2 = b2*q0
 
 Gdb = tf([q0 q1 q2], [1 -p1 -p2], T0)
 
 % DEAD BEAT DE ORDEM 3
 
-Q0 = 0.8*q0;
-Q1 = Q0*(a1-1) + 1/(b1+b2);
-Q2 = Q0*(a2-a1) + a1/(b1+b2);
-Q3 = a2*(-Q0 + 1/(b1+b2));
+Q0 = 0.8*q0
+Q1 = Q0*(a1-1) + 1/(b1+b2)
+Q2 = Q0*(a2-a1) + a1/(b1+b2)
+Q3 = a2*(-Q0 + 1/(b1+b2))
 
-P1 = Q0*b1;
-P2 = Q0*(b2 - b1) + b1/(b1+b2);
-P3 = b2*(-Q0 + 1/(b1+b2));
+P1 = Q0*b1
+P2 = Q0*(b2 - b1) + b1/(b1+b2)
+P3 = b2*(-Q0 + 1/(b1+b2))
 
 Gdb3 = tf([Q0 Q1 Q2 Q3], [1 -P1 -P2 -P3], T0)
 
@@ -73,7 +73,7 @@ xlabel('Tempo (t)')
 ylabel('Amplitude y(t)')
 legend({'GDB(2)', 'GDB(3)'}, 'Location', 'southeast');
 %axis([0 30 1 2]);
-% %exportgraphics(gca, 'Figuras/fig-erro.png');
+exportgraphics(gca, 'Figuras/fig-erro.png');
 
 figure
 stairs (out.planta2.Time, out.planta2.Data, 'b')
@@ -83,8 +83,8 @@ title('Entrada da planta do sistema Dead Beat de malha fechada')
 xlabel('Tempo (t)')
 ylabel('Amplitude y(t)')
 legend({'GDB(2)', 'GDB(3)'}, 'Location', 'southeast');
-axis([0 2 -11 11]);
-% %exportgraphics(gca, 'Figuras/fig-planta.png');
+%axis([0 2 -11 11]);
+exportgraphics(gca, 'Figuras/fig-planta.png');
 
 figure
 stairs (out.saida2.Time, out.saida2.Data, 'b')
@@ -95,9 +95,27 @@ xlabel('Tempo (t)')
 ylabel('Amplitude y(t)')
 legend({'GDB(2)', 'GDB(3)'}, 'Location', 'southeast');
 %axis([0 30 1 2]);
-%exportgraphics(gca, 'Figuras/fig-saida.png');
+exportgraphics(gca, 'Figuras/fig-saida.png');
 
 % QUESTÃO 5
+Gmfz2 = feedback(Gdb*H0GP, 1);
+Gmfz3 = feedback(Gdb3*H0GP, 1);
+
+figure
+subplot(2,1,1);
+step(Gmfz2, 'b')
+title('Resposta do sistema Dead Beat sem disturbio')
+xlabel('Tempo (t)')
+ylabel('Amplitude y(t)')
+axis([0 30 0 1.2]);
+
+subplot(2,1,2);
+step(Gmfz3, 'r')
+title('Resposta do sistema Dead Beat sem disturbio')
+xlabel('Tempo (t)')
+ylabel('Amplitude y(t)')
+axis([0 30 0 1.2]);
+
 info2 = stepinfo(out.saida2.Data, out.saida2.Time);
 fprintf("\nGBD(2): Rise Time: %.3f e Settling time: %.3f e Overshoot: %.3f.", info2.RiseTime,info2.SettlingTime, info2.Overshoot);
 
