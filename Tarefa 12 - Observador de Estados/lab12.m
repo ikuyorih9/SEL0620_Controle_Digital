c671e294e11ea46388b7fccf068eb9723a15c8b7
Hugo Nakamura <hugohiroyuki9@gmail.com>
2024-12-05 04:13:26 -0300

    Tarefa 12 completissima

--- Tarefa 12 - Observador de Estados/lab12.m
+++ Tarefa 12 - Observador de Estados/lab12.m
@@ -11,7 +11,7 @@ ess = 14.5; % erro de estado estacionário
 T0 = 0.377;
 
 % Estados iniciais
-X0_SIS = [-0.15 015];
+X0_SIS = [-0.15 0.15];
 X0_OBS = [0 0];
 
 % Espaço de estados.
@@ -21,7 +21,8 @@ Cd = [0 1.092];
 Dd = 0;
 
 K = place(F,H,[0.834+0.172i, 0.834-0.172i]);
-L = acker(F',Cd',[0, 0])
+L_T = acker(F',Cd',[0, 0])
+L = L_T'
 
 out = sim('tarefa12', 'SimulationMode', 'normal');
 
@@ -33,7 +34,7 @@ title('Estados x1(t) do sistema e do observador.')
 legend('Controlador','Observador')
 xlabel('Tempo (t)')
 ylabel('Amplitude')
-axis([0 30 -6 1])
+axis([0 30 -1 1])
 
 exportgraphics(gca, 'Figuras/Fig - x1.png');
 
@@ -45,6 +46,6 @@ title('Estados x2(t) do sistema e do observador.')
 legend('Controlador','Observador')
 xlabel('Tempo (t)')
 ylabel('Amplitude')
-axis([0 30 0 15])
+axis([0 30 0 9])
 
 exportgraphics(gca, 'Figuras/Fig - x2.png');
